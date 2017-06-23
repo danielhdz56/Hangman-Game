f5564e141290de06e242e77163a24b9e4adccbd9
Daniel Hernandez <daniel@Daniels-MacBook-Pro.local>
2017-06-23 14:42:02 -0500

    Simplified code and added Comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,105 +1,39 @@
-//Copied this Array
+//STEP1: CREATE VARIABLES
+var numberOfGuessesRemaining = 12;
+var lettersAlreadyGuessed = [];
+//STEP2: Creating Array with Country List
+//Got this online
 var countryList = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","United States Minor Outlying Islands","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
-//Made the Array LowerCase so key press will account for the first letter of the country
-var lCaseCountryList = countryList.map(function(country) {
+//making the array lowercase by using the map method
+//inside of the map method I run a function with a parameter of country
+//every element inside of my array is being fed into the "country" parameter
+//in other words the map array creates a new array with the results of calling a provided
+//function on every element in the calling array
+var lCaseCountryList = countryList.map(function(country){
+	//it is returning every index value in lowercase form
 	return country.toLowerCase();
 });
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var wins = 0;
-var currentCountry = [];
-var guessesRemaining = 12;
-var lettersGuessed = [];
-//This is a function called get All indexes
-//It has two parameters
-//arr meaning the name of the array
-//val meaning the value we want to check for in our array
-function getAllIndexes(arr, val) {
-	//This makes an empty array called indexes
-	var indexes = [];
-	//This sets a variable i to 0
-	//This is a for loop it starts at 0.
-	//It will keep going until i is smaller than the length of the array is false
-	//This will increment i by 1 at the end of the loop
-	for(i = 0; i < arr.length; i++) {
-		//The array will only run if the referenced index number of the array is equal to the value we specified in our function
-		if (arr[i] === val)
-			//This adds that value to our index array 
-			indexes.push(i);
-	}
-	return indexes;
-};
-
-
-//Created a String.replaceAt method 
-//The first parameter is the index number
-//The second parameter is what I am replacing it with
-String.prototype.replaceAt = function(index, replacement) {
-	//Remember that because the "this" keyword is used inside the function and that function is defined by the the string, 
-	//"this" refers to the String
-	//The substring is used to write the characters from 0 to the index number.
-	console.log(this);
-	return this.substr(0, index) + replacement + this.substr(index, this.length - 3);
-}
-
-
-
-
-
-
-
-//Hangman Country Generator
-var randomNumber = Math.floor(Math.random() * 208);
-//This uses the the random number to reference the country by acting as the index number
+//STEP3: GENERATING A RANDOM COUNTRY FROM ARRAY
+//3A: CREATE A RANDOM NUMBER THAT IS ONE LESS THAN THE LENGTH OF THE ARRAY (for indexing purposes)
+var randomNumber = Math.floor(Math.random() * lCaseCountryList.length);
+//3B: MATCH IT WITH THE INDEX VERSION
 var randomCountry = lCaseCountryList[randomNumber];
+//STEP4: TURN INTO HANGMAN FORM
+//The /./ matches everything
+//Future reference, If I want to use the literal . I have to escape it  /\./
+var hangmanWord = randomCountry.replace(/./gi, "_ ");
 console.log(randomCountry);
-//This splits the name of the country into letters
-var hangmanWord = randomCountry.split("");
 console.log(hangmanWord);
-//This calculates the length of the word
-var numLettersHangmanWord = hangmanWord.length;
-document.getElementById("currentCountry").innerHTML = numLettersHangmanWord;
-//Creating the Spaces for the HangmanWord
-for (var i = numLettersHangmanWord; i > 0; i--) {
-	//This appends to the array an underscore based on the length of the word
-	currentCountry.push("_");
-}
-//Makes the array into a string
-//_,_,_,_,_
-var blankSpacesCommas = currentCountry.toString();
-//This replaces commas globally, in other words every comma
-//_ _ _ _ _
-var blankSpacesNoCommas = blankSpacesCommas.replace(/,/g, " ");
-console.log(blankSpacesNoCommas);
-//This replaces commas and has no spaces either
-var blankSpacesNoCommasNoSpaces = blankSpacesCommas.replace(/,/g, "");
-document.getElementById("currentCountry").innerHTML = blankSpacesNoCommas;
+//////////////////////////////////////////////////////////////
+//STEP5: RECOGNIZE KEY INPUT FROM USER
+//You can addEventListener directly to the document, you don't have to getElementById first!
+//I am detecting for a keypress event 
+//I am going to run a function with a parameter of userInput
+//The user input "stores" the key that was pressed by the user
+document.addEventListener("keypress", function(userInput) {
+	//INSPECTING THIS IN THE CONSOLE HELPS TO UNDERSTAND WHAT THE PARAMETER IS: console.log(userInput);
+	//The String.fromCharCode method returns a string based on the value associated with the key that was pressed
+	//Note: This works if the user's browser supports keyCode or which
+	console.log(String.fromCharCode(userInput.keyCode||userInput.which));
 
-//User Input Letter Generator
-document.getElementById("hangman").addEventListener("keypress", function(event) {
-	console.log("I am listening");
-	var htmlCode = event.which || event.keyCode;
-	document.getElementById("htmlCode").innerHTML = "HTML Code:" + " " + htmlCode;
-	var alphabetNumber = htmlCode - 97;
-	document.getElementById("alphabetNumber").innerHTML = "Alphabet Number in Index:" + " " + alphabetNumber;
-	var letter = alphabet[alphabetNumber];
-	console.log(letter);
-	document.getElementById("letter").innerHTML = "Letter You Chose:" + " " + letter;
-	if (randomCountry.match(letter)) {
-		console.log("Success");
-		var successfulAttempts = getAllIndexes(hangmanWord, letter);
-		console.log(successfulAttempts);
-		var updatedHangman = blankSpacesNoCommasNoSpaces.replaceAt(successfulAttempts, letter);
-		console.log(updatedHangman);
-	}
-	else {
-		console.log("Fail");
-	}
 });
-
-
-
-
-
-
-
-
